74063c674abe4a6212350307033042adf593619b
Krishnapriya-prasannan <krishnapriyaprasannan1@gmail.com>
2023-12-25 19:42:21 +0530

    Nav and Events modified

--- EventCalender/src/App.jsx
+++ EventCalender/src/App.jsx
@@ -3,9 +3,7 @@ import Navbar from './Components/Navbar';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import UpcomingEvents from './Components/UpcomingEvents';
 
-const Home = () => <div>Home Component</div>;
-const Clubs = () => <div>Clubs Component</div>;
-const AboutUs = () => <div>About Us Component</div>;
+
 
 function App() {
   return (
@@ -13,13 +11,6 @@ function App() {
     <Router>
       <>
         <Navbar />
-        <Routes>
-          {/* Define your routes using the Route component */}
-          <Route path="/" element={<Home />} />
-          <Route path="/clubs" element={<Clubs />} />
-          <Route path="/about-us" element={<AboutUs />} />
-          {/* Add more routes as needed */}
-        </Routes>
         
       </>
     </Router>
--- EventCalender/src/Components/Navbar.jsx
+++ EventCalender/src/Components/Navbar.jsx
@@ -1,34 +1,32 @@
-// Navbar.jsx
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 const Navbar = () => {
   return (
-    <nav>
+    <nav className="navbar">
       <div className="logo">Logo</div>
       <div className="nav-links">
-      <Link to="/">Home</Link>
-        <Link to="/upcoming-events">Upcoming Events</Link>
-        <Link to="/about-us">About Us</Link>
-        <Link to="/contact-us">Contact Us</Link>
-        
+        <Link to="/home" className="nav-link">Home</Link>
+        <Link to="/upcoming-events" className="nav-link">Upcoming Events</Link>
+        <Link to="/about-us" className="nav-link">About Us</Link>
+        <Link to="/contact-us" className="nav-link">Contact Us</Link>
       </div>
       <div className="auth-links">
         <div className="auth-dropdown">
-          <span>Clubs</span>
+          <span className="dropdown-trigger">Clubs</span>
           <div className="dropdown-content">
-            <Link to="/ieee">IEEE</Link>
-            <Link to="/gdsc">GDSC</Link>
+            <Link to="/ieee" className="dropdown-item">IEEE</Link>
+            <Link to="/gdsc" className="dropdown-item">GDSC</Link>
           </div>
         </div>
       </div>
       <div className="auth-links">
         <div className="auth-dropdown">
-          <span>Login/Signup</span>
+          <span className="dropdown-trigger">Login/Signup</span>
           <div className="dropdown-content">
-            <Link to="/login">Login</Link>
-            <Link to="/signup">Signup</Link>
+            <Link to="/login" className="dropdown-item">Login</Link>
+            <Link to="/signup" className="dropdown-item">Signup</Link>
           </div>
         </div>
       </div>
