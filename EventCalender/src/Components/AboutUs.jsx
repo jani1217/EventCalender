4dd8628c0c03e76f593fdfa9c23a3abf9df575ae
jani1217 <125665133+jani1217@users.noreply.github.com>
2023-12-27 16:03:15 +0530

    about us

---
+++ EventCalender/src/Components/AboutUs.jsx
@@ -0,0 +1,30 @@
+import './AboutUs.css';
+const AboutUs = () =>{
+return(
+    <div className="about">
+        <div className="photo">
+   <img src="https://tse3.mm.bing.net/th?id=OIP.h-g6x28CCi-SMzfGWdfzoQHaHa&pid=Api&P=0&h=180"></img>
+   <img src="https://tse3.mm.bing.net/th?id=OIP.QXkORNsTeQu-5EBDweBRSgHaHa&pid=Api&P=0&h=180"></img>
+   <img src="https://cdn0.iconfinder.com/data/icons/office-50/512/mar-1024.png"></img>
+   <img src="https://cdn0.iconfinder.com/data/icons/office-50/512/apr-1024.png"></img> 
+   <img src="https://cdn0.iconfinder.com/data/icons/office-50/512/may-1024.png"></img>
+   <img src="https://cdn0.iconfinder.com/data/icons/office-50/512/june-1024.png"></img>
+   <img src="https://cdn0.iconfinder.com/data/icons/office-50/512/july-1024.png"></img>
+   <img src="https://cdn0.iconfinder.com/data/icons/office-50/512/aug-1024.png"></img>
+   <img src="https://cdn0.iconfinder.com/data/icons/office-50/512/sept-1024.png"></img>
+   <img src="https://cdn0.iconfinder.com/data/icons/office-50/512/oct-1024.png"></img>
+   <img src="https://cdn0.iconfinder.com/data/icons/office-50/512/nov-1024.png"></img>
+   <img src="https://cdn0.iconfinder.com/data/icons/office-50/512/dec-1024.png"></img>
+   
+        </div>
+    <div className="content">   
+  <h1>ABOUT US</h1>
+  
+
+  <p>We GDSC MACE presents to you the Event Calender</p>
+  <p>Event Clender displays events hosted by all the clubs currently active in MACE  </p>
+  </div> 
+</div>);
+};
+
+export default AboutUs;
