8cf99fb4923cecda934057e472ac0f848682091e
Krishnapriya-prasannan <krishnapriyaprasannan1@gmail.com>
2023-12-28 09:43:33 +0530

    Navbar updated

---
+++ EventCalender/tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundColor: {
+        '023440': '#023440',
+      },
+    },
+  },
+  plugins: [],
+}
